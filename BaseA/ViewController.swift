87d17900e3d5256203a4342e7db82fcb3741c819
youyou5920 <youyou5920@live.com>
2018-03-02 10:58:37 +0800

    update
    first commit

---
+++ BaseA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseA
+//
+//  Created by chen on 2018/3/1.
+//  Copyright © 2018年 Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        BaseA().message()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
